a5a7a965077b1af19d458d01f9b89fc574958e5d
Konstantin Panov <konst.panov@gmail.com>
2024-12-25 15:41:29 +0300

    Мой второй коммит

--- models/trips_stat.sql
+++ models/trips_stat.sql
@@ -1,19 +1,9 @@
-select 
-  count(*) as trips, 
-  count(DISTINCT user_id) as users, 
-  avg(
-    EXTRACT(
-      EPOCH 
-      FROM 
-        (finished_at - started_at)/ 60
-    )
-  ) as avg_duration_m, 
-  sum(price) / 100 as revenue_rub, 
-  (
-    count(*) FILTER (
-      WHERE 
-        price = 0
-    ):: decimal / count(*):: decimal
-  )* 100 as free_trips_pct 
-from 
-  scooters_raw.trips
+select
+    count(*) as trips,
+    count(distinct user_id) as users,
+    avg(duration_s) / 60 as avg_duration_min,
+    sum(distance_m) / 1000 as sum_distance_km,
+    sum(price_rub) as revenue_rub,
+    count(is_free or null) / cast(count(*) as real) * 100 as free_trips_pct
+from
+    {{ ref("trips_prep") }}
